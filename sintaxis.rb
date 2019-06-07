a7cad1f6dee510633a00daf12c7dddb88fb71cf0
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2019-06-07 19:52:47 -0400

    finished exercise

--- sintaxis.rb
+++ sintaxis.rb
@@ -1,13 +1,16 @@
 class Anything
   def foo
-    a = 5
+    @a = 5
   end
   def bar
     @a += 1
+  end
+  def mostrar_any()
+    puts @a
   end
 end
 
 any = Anything.new
 any.foo
 any.bar
-any.a
+any.mostrar_any
