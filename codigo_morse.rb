744c319aa9e9be03a89aeee627083146c125ea39
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2019-06-07 21:34:12 -0400

    finish with morse code

--- codigo_morse.rb
+++ codigo_morse.rb
@@ -3,17 +3,18 @@ class Morseable
     @number = number
   end
 def generate_hash(number)
-  # Esto es una aberración y debe ser refactorizado!
-  h = '-----' if number == 0
-  h = '.----' if number == 1
-  h = '..---' if number == 2
-  h = '...--' if number == 3
-  h = '....-' if number == 4
-  h = '.....' if number == 5
-  h = '-....' if number == 6
-  h = '--...' if number == 7
-  h = '---..' if number == 8
-  h = '----.' if number == 9
+  h = {0 => '-----',
+       1 => '.----',
+       2 => '..---',
+       3 => '...--',
+       4 => '....-',
+       5 => '.....',
+       6 => '-....',
+       7 => '--...',
+       8 => '---..',
+       9 => '----.'
+     }
+     h.values[number]
 end
 
 def to_morse
@@ -21,5 +22,5 @@ def to_morse
   end
 end
 
-m = Morseable.new(3)
-print m.to_morse
+m = Morseable.new(4)
+puts m.to_morse
