9e64facb93020dc41bc5c9eb1f9a66aea78ae34e
Rodrigo <rodrigo.lindenmeyer.toledo@gmail.com>
2019-06-07 20:28:50 -0400

    done with the third

--- sintaxis3.rb
+++ sintaxis3.rb
@@ -1,8 +1,9 @@
 class Store
+  attr_reader :name
   def initialize(name)
-    name = @name
+    @name = name
   end
 end
 
-store = store.new('Tienda 1')
+store = Store.new('Tienda 1')
 puts store.name
